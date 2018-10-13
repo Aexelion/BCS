38d9996b881d70169e767a10c2a72fe751118192
Aexelion <aexelion@gmail.com>
2018-10-13 18:23:15 +0200

    Ajout de la fonction f de Keccak

--- Sha3_perso.py
+++ Sha3_perso.py
@@ -2,12 +2,12 @@
 
 ##Sha3-512 : r = 512; c = 1024
 ##Sha3-384 : r = 832; c = 768
-##Sha3-384 : r = 1088; c = 512
-##Sha3-384 : r = 1152; c = 448
+##Sha3-256 : r = 1088; c = 512
+##Sha3-224 : r = 1152; c = 448
 
 
 def padding(M,r):
-	"""Ajoute du padding a un message quelconque afin de le rendre de taille multiple de r"""
+	"""Ajoute du padding a un message quelconque afin de le rendre de taille multiple de r."""
 	
 	val = len(M + '11') % r
 	res = M + '1' + '0'*((r-val)%r) + '1'
@@ -15,7 +15,7 @@ def padding(M,r):
 
 
 def toBlock(M, r):
-	"""Transforme un message de taille multiple de r en un tableau de partie de message tous de taille r"""
+	"""Transforme un message de taille multiple de r en un tableau de partie de message tous de taille r."""
 	
 	k = len(M) // r
 	m = [M[r*i:(r+1)*i] for i in range(k)]
@@ -23,7 +23,7 @@ def toBlock(M, r):
 
 
 def xor(block, m, r):
-	"""Realise un xor bit a bit entre un block de taille > r et un message de taille r"""
+	"""Realise un xor bit a bit entre un block de taille > r et un message de taille r."""
 	
 	res = [block[i] for i in range(len(block))]
 	for i in range(r):
@@ -32,18 +32,28 @@ def xor(block, m, r):
 
 
 def transformation(block):
-	"""Transforme un block de 1600 bits en une matrice 3 dimension 5*5*64"""
+	"""Transforme un block de 1600 bits en une matrice 3 dimension 5*5*64."""
 	
 	res = [['0'*64 for i in range(5)] for i in range(5)]
 	for ligne in range(5):
 		for colonne in range(5):
-			res[ligne][colonne] = block[ligne*320+colonne*64:
+			res[ligne][colonne] = block[ligne*320+colonne*64 :
 			64+ligne*320+colonne*64]
 	return res
 
 
+def detransformation(matrice):
+	"""Transforme une matrice 3 dimension 5*5*64 en un block de 1600 bits."""
+	
+	res = ''
+	for ligne in range(5):
+		for colonne in range(5):
+			res += matrice[ligne][matrice]
+	return res
+
+
 def cp(matrice, x, z):
-	"""Calcul de la parité de la colonne situé à l'emplacement x, z"""
+	"""Calcul de la parité de la colonne situé à l'emplacement x, z."""
 	
 	res = str(
 	int(matrice[x][0][z]) ^
@@ -56,7 +66,7 @@ def cp(matrice, x, z):
 
 
 def theta(matrice):
-	"""Fonction theta de Keccak realisé sur une matrice de 1600 bits découpé en matrice 5*5*64. Réalisation d'un ensemble de xor sur les différents bits de la matrice"""
+	"""Fonction theta de Keccak realisé sur une matrice de 1600 bits découpé en matrice 5*5*64. Réalisation d'un ensemble de xor sur les différents bits de la matrice."""
 	
 	res = [['0'*64 for i in range(5)] for i in range(5)]
 	for x in range(5):
@@ -110,16 +120,35 @@ def khi(matrice):
 		return res
 
 
-def iota(matrice, roundNb):
-	"""Fonction iota de Keccak réalisé sur une matrice de 1600 bits découpée en matrice 5*5*64. Ajout d'une constante sur la 1ere ligne de la matrice (située en 0,0)."""
+def hexToBin(hexa):
+	"""Transforme un nombre en hexadecimal en un mot de 64 bits."""
 	
-	RC = ['0'*64 for i in range(24)] #TODO trouver les valeurs de RC
+	res = bin(hexa)[2:]
+	res = '0'*(64-len(res))+res
+	return res
+
+
+def iota(matrice, roundNb):
+	"""Fonction iota de Keccak réalisé sur une matrice de 1600 bits découpée en matrice 5*5*64. Ajout d'une constante sur la 1ere ligne de la matrice 5*5 (située en 0,0)."""
+	
+	RC = [
+	0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+	0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+	0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+	0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+	0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+	0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+	0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+	0x8000000000008080, 0x0000000080000001, 0x8000000080008008]
+	
+	binRC = hexToBin(RC[roundNb])
+	
 	res = [[matrice[i][j] for i in range(5)] for j in range(5)]
 	for z in range(64):
 		res[0][0] = res[0][0][:z] + \
 		str(
 		int(matrice[0][0][z]) ^
-		RC[0][z]
+		int(binRC[z])
 		) + \
 		res[0][0][(z+1):]
 	return res
@@ -127,8 +156,16 @@ def iota(matrice, roundNb):
 
 
 def f(block):
-	"""Fonction de permutation de Keccak"""
-	pass
+	"""Fonction de permutation de Keccak."""
+	matrice = transformation(block)
+	for i in range(24):
+		matrice = theta(matrice)
+		matrice = rho(matrice)
+		matrice = pi(matrice)
+		matrice = khi(matrice)
+		matrice = iota(matrice, i)
+	res = detransformation(matrice)
+	return res
 
 if __name__ == "__main__" :
 	init = '1'+'0'*1599
