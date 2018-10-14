a3f80971ea35a39720ba16ec26db899aee04f1f2
Aexelion <aexelion@gmail.com>
2018-10-14 16:05:45 +0200

    Chiffrement midori OK

--- Midori64_perso.py
+++ Midori64_perso.py
@@ -8,16 +8,16 @@ p = [0, 10, 5, 15, 14, 4, 11, 1, 9, 3, 12, 6, 7, 13, 2, 8]
 
 #M = [0,1,1,1, 1,0,1,1, 1,1,0,1, 1,1,1,0]
 
-a0 = [0,0,0,1, 0,1,0,1, 1,0,1,1, 0,0,1,1]
-a1 = [0,1,1,1, 1,0,0,0, 1,1,0,0, 0,0,0,0]
-a2 = [1,0,1,0, 0,1,0,0, 0,0,1,1, 0,1,0,1]
-a3 = [0,1,1,0, 0,0,1,0, 0,0,0,1, 0,0,1,1]
-a4 = [0,0,0,1, 0,0,0,0, 0,1,0,0, 1,1,1,1]
-a5 = [1,1,0,1, 0,0,0,1, 0,1,1,1, 0,0,0,0]
-a6 = [0,0,0,0, 0,0,1,0, 0,1,1,0, 0,1,1,0]
-a7 = [0,0,0,0, 1,0,1,1, 1,1,0,0, 1,1,0,0]
-a8 = [1,0,0,1, 0,1,0,0, 1,0,0,0, 0,0,0,1]
-a9 = [0,1,0,0, 0,0,0,0, 1,0,1,1, 1,0,0,0]
+a0  = [0,0,0,1, 0,1,0,1, 1,0,1,1, 0,0,1,1]
+a1  = [0,1,1,1, 1,0,0,0, 1,1,0,0, 0,0,0,0]
+a2  = [1,0,1,0, 0,1,0,0, 0,0,1,1, 0,1,0,1]
+a3  = [0,1,1,0, 0,0,1,0, 0,0,0,1, 0,0,1,1]
+a4  = [0,0,0,1, 0,0,0,0, 0,1,0,0, 1,1,1,1]
+a5  = [1,1,0,1, 0,0,0,1, 0,1,1,1, 0,0,0,0]
+a6  = [0,0,0,0, 0,0,1,0, 0,1,1,0, 0,1,1,0]
+a7  = [0,0,0,0, 1,0,1,1, 1,1,0,0, 1,1,0,0]
+a8  = [1,0,0,1, 0,1,0,0, 1,0,0,0, 0,0,0,1]
+a9  = [0,1,0,0, 0,0,0,0, 1,0,1,1, 1,0,0,0]
 a10 = [0,1,1,1, 0,0,0,1, 1,0,0,1, 0,1,1,1]
 a11 = [0,0,1,0, 0,0,1,0, 1,0,0,0, 1,1,1,0]
 a12 = [0,1,0,1, 0,0,0,1, 0,0,1,1, 0,0,0,0]
@@ -26,9 +26,10 @@ a14 = [1,1,0,1, 1,1,1,1, 1,0,0,1, 0,0,0,0]
 alpha = [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14]
 
 resultat0 = [0x3, 0xc, 0x9, 0xc, 0xc, 0xe, 0xd, 0xa, 0x2, 0xb, 0xb, 0xd, 0x4, 0x4, 0x9, 0xa]
+resultat1 = [0x6, 0x6, 0xb, 0xc, 0xd, 0xc, 0x6, 0x2, 0x7, 0x0, 0xd, 0x9, 0x0, 0x1, 0xc, 0xd]
 
 def subCell(mot):
-	return Sbox1[mot]	
+	return Sbox1[mot]
 
 def subCells(matrice):
 	res = [0 for i in range(16)]
@@ -39,13 +40,13 @@ def subCells(matrice):
 def shuffleCells(matrice):
 	res = [0 for i in range(16)]
 	for i in range(16):
-		res[p[i]] = matrice[i]
+		res[i] = matrice[p[i]]
 	return res
 
 def mixColumn(matrice):
 	res = [0 for i in range(16)]
 	for i in range(4):
-		res[4*i] = matrice[1+4*i] ^ matrice[2+4*i] ^ matrice[3+4*i]
+		res[0+4*i] = matrice[1+4*i] ^ matrice[2+4*i] ^ matrice[3+4*i]
 		res[1+4*i] = matrice[0+4*i] ^ matrice[2+4*i] ^ matrice[3+4*i]
 		res[2+4*i] = matrice[0+4*i] ^ matrice[1+4*i] ^ matrice[3+4*i]
 		res[3+4*i] = matrice[0+4*i] ^ matrice[1+4*i] ^ matrice[2+4*i]
@@ -75,25 +76,25 @@ def midori(m, k):
 
 
 if __name__ == '__main__':
-	print(midori([0x0 for i in range(16)],[0x0 for i in range(32)]))
+	res0 = midori([0x0 for i in range(16)],[0x0 for i in range(32)])
+	print(res0)
 	print(resultat0)
+	print('\n')
+	res1 = midori(
+	[0x4, 0x2, 0xc, 0x2,
+	 0x0, 0xf, 0xd, 0x3,
+	 0xb, 0x5, 0x8, 0x6,
+	 0x8, 0x7, 0x9, 0xe]
+	,
+	[0x6, 0x8, 0x7, 0xd,
+	 0xe, 0xd, 0x3, 0xb,
+	 0x3, 0xc, 0x8, 0x5,
+	 0xb, 0x3, 0xf, 0x3,
+	 
+	 0x5, 0xb, 0x1, 0x0,
+	 0x0, 0x9, 0x8, 0x6,
+	 0x3, 0xe, 0x2, 0xa,
+	 0x8, 0xc, 0xb, 0xf])
+	print(res1)
+	print(resultat1)
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
