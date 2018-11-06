fbda9c712cf3f5fac30d0a55a6036aaf2561e31c
Aexelion <aexelion@gmail.com>
2018-11-06 10:41:34 +0100

    Mode Opératoire CTR

---
+++ ModeOp.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python3
+
+from Midori64_perso import midori, antiMidori
+from Sha3_perso import keccak
+import trad
+
+
+def decoupe(hexa):
+	if len(hexa)%16 != 0 :
+		hexa += '0'*(16-len(hexa)%16)
+	res = [hexa[16*i : 16*(i+1)] for i in range(len(hexa)//16)]
+	return res
+
+
+def convert(nb):
+	res = hex(nb)[2:]
+	l = len(res)
+	if l < 8:
+		res = '0'*(8-l) + res
+	return res[:8]
+
+
+def CTR(nonce, message, key, mode=0):
+	ctr = 0
+	hexa = message
+	if mode == 0:
+		hexa = trad.strToHex(hexa)
+	M = decoupe(hexa)
+	C = []
+	strNonce = convert(nonce)
+	for Mi in M:
+		strCtr = convert(ctr)
+		strInput = strNonce + strCtr
+		Input = [int(strInput[i], 16) for i in range(len(strInput))]
+		tmp = midori(Input, key)
+		Ci = ''
+		for j in range(16):
+			Ci += hex(tmp[j] ^ int(Mi[j], 16))[2:]
+		C.append(Ci)
+		ctr += 1
+	res = ''
+	for Ci in C:
+		res += Ci
+	return res
+
+
+if __name__ == '__main__' :	
+	nonce = 123456789
+	key = [0 for i in range(32)]
+	message = 'abcdefghijklmnopqrstuvwxz0123456789ABCDEFGHIJKLMNOPQRSTUVWXZ'
+	tmp = CTR(nonce, message, key, 0)
+	print("Message après chiffrement (Midori - CTR) :", tmp)
+	tmp2 = CTR(nonce, tmp, key, 1)
+	print("Message après déchiffrement (Midori - CTR) :", tmp2)
+	print("Traduction inverse du déchiffrement :", trad.hexToStr(tmp2))
+	
