8707937cb4f5e52029ad676fde251ed23afa14d4
Aexelion <aexelion@gmail.com>
2018-11-13 20:47:52 +0100

    Calcul du MAC

--- MAC_perso.py
+++ MAC_perso.py
@@ -5,29 +5,48 @@ import trad
 
 
 def decoupe(hexa):
+	pad = False
 	if len(hexa)%16 != 0 :
-		hexa += '0'*(16-len(hexa)%16)
+		hexa += '8' + '0'*(16-(len(hexa)+1)%16)
+		pad = True
 	res = [hexa[16*i : 16*(i+1)] for i in range(len(hexa)//16)]
+	return res, pad
+
+
+def lastBlock(block, key, pad):
+	input0 = [0 for i in range(16)]
+	k0 = midori(input0, key)
+	k1 = [k0[i] for i in range(15)]
+	k1.append(0)
+	if k0[0] >= 8 :
+		k1[14] = k1[14] ^ 0x1
+		k1[15] = k1[15] ^ 0xB
+	k2 = [k1[i] for i in range(15)]
+	k2.append(0)
+	if k1[0] >= 8 :
+		k2[14] = k2[14] ^ 0x1
+		k2[15] = k2[15] ^ 0xB
+	tmp = [int(block[i],16) for i in range(16)]
+	res = ''
+	if pad :
+		for i in range(16):
+			res += hex(tmp[i] ^ k2[i])[2:]
+	else :
+		for i in range(16):
+			res += hex(tmp[i] ^ k1[i])[2:]
 	return res
 
 
-def CMAC(hexa, key1, key2):
-	M = decoupe(hexa)
+def CMAC(hexa, key):
+	M, pad = decoupe(hexa)
 	res = '0'*16
+	M[-1] = lastBlock(M[-1], key, pad)
 	for Mi in M:
 		Input = [int(Mi[i], 16) ^ int(res[i], 16) for i in range(16)]
-		tmp = midori(Input, key1)
+		tmp = midori(Input, key)
 		res = ''
 		for i in tmp:
 			res += hex(i)[2:]
-	tmp = ''
-	for i in range(16):
-		tmp += hex(int(res[i], 16) ^ key2[i])[2:]
-	Input = [int(tmp[i], 16) for i in range(16)]
-	tmp = midori(Input, key1)
-	res = ''
-	for i in tmp:
-		res += hex(i)[2:]
 	return res
 
 
@@ -36,5 +55,5 @@ if __name__ == '__main__' :
 	key = [0 for i in range(32)]
 	message = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 	hexa = trad.strToHex(message)
-	mac = CMAC(hexa, key, key)
+	mac = CMAC(hexa, key)
 	print("MAC du message :", mac)
