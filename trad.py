ffab45abfeef39d39caea30ab40cfe6190d004bb
Aexelion <aexelion@gmail.com>
2018-11-24 16:47:06 +0100

    Traduction OK (commentaires)

--- trad.py
+++ trad.py
@@ -2,14 +2,16 @@
 
 
 def strToHex(s):
+	"""Fonction de traduction d'une chaîne de caractère vers une chaîne en hexadécimal."""
 	res = ''
 	for i in s:
 		tmp = hex(ord(i))[2:]
-		res += '0'*(2-len(tmp)) + tmp
+		res += '0'*(len(tmp)%2) + tmp
 	return res
 
 
 def hexToStr(h):
+	"""Fonction de traduction d'une chaîne en hexadécimal vers une chaîne de caractère."""
 	res = ''
 	for i in range(0, len(h), 2) :
 		res += chr(int(h[i:i+2], 16))
@@ -17,6 +19,7 @@ def hexToStr(h):
 
 
 def strToBin(s):
+	"""Fonction de traduction d'une chaîne de caractère vers une chaîne en binaire."""
 	res = ''
 	for i in s:
 		tmp = bin(ord(i))[2:]
@@ -25,6 +28,7 @@ def strToBin(s):
 
 
 def binToStr(b):
+	"""Fonction de traduction d'une chaîne en binaire vers une chaîne de caractère."""
 	res = ''
 	for i in range(0, len(b), 8):
 		res += chr(int(b[i:i+8], 2))
@@ -32,6 +36,7 @@ def binToStr(b):
 
 
 def binToHex(b):
+	"""Fonction de traduction d'une chaîne en binaire vers une chaîne en hexadécimal."""
 	res = ''
 	for i in range(0, len(b), 8):
 		res += hex(int(b[i:i+8], 2))[2:]
@@ -39,6 +44,7 @@ def binToHex(b):
 
 
 def hexToBin(h):
+	"""Fonction de traduction d'une chaîne en hexadécimal vers une chaîne en binaire."""
 	res = ''
 	for i in range(0, len(h), 2) :
 		res += bin(int(h[i:i+2], 16))[2:]
